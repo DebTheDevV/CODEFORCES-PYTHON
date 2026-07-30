14090631cf04e2ca09e3e847ab64c4659eb97f15
DebTheDevV <debthedevv@gmail.com>
2026-07-30 21:11:14 +0530

    day11-281A

---
+++ day11.py
@@ -0,0 +1,8 @@
+# Read the input string
+s = input()
+
+# Capitalize the first letter and concatenate the rest of the string
+result = s[0].upper() + s[1:]
+
+# Print the final result
+print(result)
